d4aa2895dd2045b5e483b89f2eabc1088007638f
Rommel Terrence Juanillo <terrence@newlogic.com>
2022-09-09 15:05:05 +0800

    chore: Add docs for openapi in card viewset
    Signed-off-by: Rommel Terrence Juanillo <terrence@newlogic.com>

--- card_generator/api/v1/cards/views.py
+++ card_generator/api/v1/cards/views.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -9,19 +9,27 @@ from card_generator.cards.models import Card
 
 
 @extend_schema(tags=["cards"])
+@extend_schema_view(
+    list=extend_schema(description="List all available card templates."),
+    retrieve=extend_schema(description="Retrieve a card template."),
+    create=extend_schema(description="Createa a new card template."),
+    update=extend_schema(description="Update a card template."),
+    delete=extend_schema(description="Remove a card template."),
+)
 class CardViewSet(ModelViewSet):
 
     serializer_class = CardSerializer
     queryset = Card.objects.all()
     lookup_field = "uuid"
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = (TokenAuthentication,)
+    http_method_names = ("get", "post", "put", "delete")
 
     @action(
         methods=["post"],
         detail=True,
     )
     def render(self, request, **kwargs):
-        """Apply values to card templates and render it in pdf and png formats."""
+        """Generate a card from a template with the provided values."""
         serializer = CardRenderSerializer(
             data=request.data, context={"card": self.get_object()}
         )
@@ -33,6 +41,6 @@ class CardViewSet(ModelViewSet):
         detail=True,
     )
     def fields(self, request, **kwargs):
-        """List all fields available for update."""
+        """List all variable fields present in the template where the user can provide a value."""
         data = {"fields": self.get_object().get_fields()}
         return Response(data=data)
