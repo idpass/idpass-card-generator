2fc61f48dafe8fa8b8ef26dab553110913414d03
Rommel Terrence Juanillo <terrence@newlogic.com>
2022-10-06 17:06:17 +0800

    feat: Remove logging disable; Add collectstatic command
    Signed-off-by: Rommel Terrence Juanillo <terrence@newlogic.com>

--- config/settings/production.py
+++ config/settings/production.py
@@ -108,7 +108,7 @@ ANYMAIL = {}
 
 LOGGING = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "verbose": {
             "format": "%(levelname)s %(asctime)s %(module)s "
